8b39831a53d2b5b7fbdbac9b24a4994c21374c9a
christalent1 <chrislytalent766@gmail.com>
2023-08-24 14:34:40 +0100

    start nodejs

---
+++ global.js
@@ -0,0 +1,9 @@
+setTimeout(() =>{
+
+    console.log('we are coding nobejs')
+
+} , 1000);
+
+let int = setInterval(() =>{
+
+}); 
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['olu','john','kenneth','daniel','dan']
+const age = [23,34,24,45,6,4,5]
+
+
+console.log(people)
+
+module.exports = people
